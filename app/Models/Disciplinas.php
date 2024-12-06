35d209df1eaaac9d9faaac966be601b41e15f946
deividinacio <deivid_@outlook.com>
2024-12-06 15:09:23 -0300

    adicionado softdeletes para Disciplinas

--- app/Models/Disciplinas.php
+++ app/Models/Disciplinas.php
@@ -3,10 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Disciplinas extends Model
 {
-    //use SoftDeletes;
+    use SoftDeletes;
 
     protected $table = 'disciplinas';
     protected $fillable = ['nome'];
